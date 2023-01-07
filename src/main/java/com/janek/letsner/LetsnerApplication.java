b85f90d3e21d31653e6ac3f58d9bb79d3d54c600
Janek <eunho.moon@gmail.com>
2023-01-07 15:52:41 +0900

    프로젝트 세팅

---
+++ src/main/java/com/janek/letsner/LetsnerApplication.java
@@ -0,0 +1,13 @@
+package com.janek.letsner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetsnerApplication.class, args);
+    }
+
+}
