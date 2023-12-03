e2f91d260ced182a34e0076c30ca81007a2aa1c8
Janek <eunho.moon@gmail.com>
2023-12-04 00:43:26 +0900

    [feat] Add sample controller and h2/jpa/jpa auditing config

---
+++ src/main/java/com/janek/letsner/config/JpaAuditingConfiguration.java
@@ -0,0 +1,9 @@
+package com.janek.letsner.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+}
