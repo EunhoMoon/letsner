dd825b77d7275b2b6bf06e7023766e8ffcfdf69c
Janek <eunho.moon@gmail.com>
2023-12-21 23:05:24 +0900

    [feat] add lesson kind and lesson type

--- src/main/java/com/janek/letsner/domain/lesson/LessonKind.java
+++ src/main/java/com/janek/letsner/domain/lesson/LessonKind.java
@@ -5,8 +5,8 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public enum LessonType {
-    ACADEMY("학원 레슨"), 
+public enum LessonKind {
+    ACADEMY("학원 레슨"),
     PRIVATE("개인 레슨"),
     GROUP("그룹 레슨"),
     ONLINE("온라인 레슨"),
--- src/main/java/com/janek/letsner/domain/lesson/LessonType.java
+++ src/main/java/com/janek/letsner/domain/lesson/LessonType.java
@@ -1,2 +1,12 @@
-package com.janek.letsner.domain.lesson;public enum LessonType {
+package com.janek.letsner.domain.lesson;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum LessonType {
+    REGULAR("정규"), SUPPLEMENTARY("보강");
+
+    private final String description;
 }
